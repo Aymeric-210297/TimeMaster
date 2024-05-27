0e00b564fcf8ce41d4b9503714a38a8df966237b
Aymeric-210297 <114666523+Aymeric-210297@users.noreply.github.com>
2024-05-27 06:06:44 +0200

    Correction erreur JS si aucun flash message

--- public/assets/scripts/flashMessage.js
+++ public/assets/scripts/flashMessage.js
@@ -1,6 +1,8 @@
 const flashMessage = document.getElementById("flash-message");
 const closeFlashMessage = document.getElementById("close-flash-message");
 
-closeFlashMessage.addEventListener("click", function () {
-  document.body.removeChild(flashMessage);
-});
+if (flashMessage && closeFlashMessage) {
+    closeFlashMessage.addEventListener("click", function () {
+    document.body.removeChild(flashMessage);
+    });
+}
