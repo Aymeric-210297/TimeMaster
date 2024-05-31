b70b289d982b3d5577cd335340fdcb82e7766a56
Aymeric-210297 <114666523+Aymeric-210297@users.noreply.github.com>
2024-05-31 13:19:53 +0200

    Ajout d'icônes par défaut sur les flash messages

--- utils/functions/createFlashMessage.php
+++ utils/functions/createFlashMessage.php
@@ -1,7 +1,24 @@
 <?php
 
-function createFlashMessage($title, $message, $icon = "fa-solid fa-bell", $type = "primary")
+function createFlashMessage($title, $message, $type = "primary", $icon = null)
 {
+    if (empty($icon)) {
+        switch($type) {
+            case 'error':
+                $icon = "fa-solid fa-circle-exclamation";
+                break;
+            case 'warning':
+                $icon = "fa-solid fa-triangle-exclamation";
+                break;
+            case 'success':
+                $icon = "fa-solid fa-circle-check";
+                break;
+            default:
+                $icon = "fa-solid fa-bell";
+                break;
+        }
+    }
+
     $_SESSION['flashMessage'] = [
         'title' => $title,
         'message' => $message,
